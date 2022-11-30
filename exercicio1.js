a26af9ab962a8cb0d50d3d9ad28353798c838233
Pietro Klein <117283920+pietrokp@users.noreply.github.com>
2022-11-30 20:21:02 -0300

    Update exercicio1.js

--- exercicio1.js
+++ exercicio1.js
@@ -1,4 +1,15 @@
+/*
+    Nesse desafio, você irá solicitar ao usuário que ele insira **dois números** e, a partir daí, calcular:
+    - A soma dos dois números;
+    - A subtração dos dois números;
+    - A multiplicação dos dois números;
+    - A divisão dos dois números;
+    - O resto da divisão dos dois números;
 
+    Extra:
+    - Verifique se a soma dos dois números é par (ou ímpar).
+    - Verifique se os dois números inseridos são iguais (ou diferentes).
+*/
 
 alert('Por favor, selecione dois números para iniciar a aplicação.')
 
